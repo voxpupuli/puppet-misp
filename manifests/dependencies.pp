c99d013daa0a191eef8e6de616833e2566b5c54e
Pablo Panero <pablo.panero@cern.ch>
2016-08-24 14:19:35 +0200

    services changed from dependencies to install

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -31,19 +31,4 @@ class misp::dependencies inherits misp {
     unless => "/usr/bin/pip list | grep importlib",
   }
 
-  service { 'rh-php56-php-fpm':
-    enable => true,
-    ensure => 'running',
-    subscribe => File['/etc/opt/rh/rh-php56/php.d/99-redis.ini'], #Needs the subscribe, cannot notify a service
-  }
-
-  service { 'haveged':
-    enable => true,
-    ensure => 'running',
-  }
-
-  service { 'redis':
-    enable => true,
-    ensure => 'running',
-  }
 }
