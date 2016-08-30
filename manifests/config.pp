65da5725d8264a4010b7d10689f6277f0203996e
Pablo Panero <pablo.panero@cern.ch>
2016-08-30 14:32:57 +0200

    certmgt commented + defaults on config done properly

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,7 +1,9 @@
 
-class misp::config ($db_name = 'default', $db_user = 'default', $db_host = 'default', $db_port = 'default', $git_tag='default', $salt='default', $cipherseed='default',
-  $ssl_port = 'default', $server_admin = 'deafault', $document_root = 'default', $certificate_file ='default',
-  $certificate_key_file = 'default', $certificate_chain_file = 'default', $orgname = 'default', $webservername = 'default') inherits misp {
+class misp::config ($db_name = 'misp', $db_user = 'misp', $db_host = 'misp.com', $db_port = '5505', $git_tag='v2.4.51',
+  $salt='Rooraenietu8Eeyo<Qu2eeNfterd-dd+', $cipherseed='',
+  $ssl_port = '443', $server_admin = 'root@localhost', $document_root = '/var/www/html',
+  $certificate_file ='hostcert.pem', $certificate_key_file = 'hostkey.pem', $certificate_chain_file = 'ca.pem',
+  $orgname = 'ORGNAME', $webservername = 'httpd') inherits misp {
 
   require '::misp::install'
 
@@ -100,7 +102,6 @@ class misp::config ($db_name = 'default', $db_user = 'default', $db_host = 'defa
     mode       => '640',
   }
 
-
   file{'/var/www/MISP/app/Config/config.php':
     ensure    => file,
     owner     => 'apache',
@@ -110,56 +111,9 @@ class misp::config ($db_name = 'default', $db_user = 'default', $db_host = 'defa
     subscribe => Exec['Directory permissions'],
   }
 
-  #exec {'chcon files':
-  #  command   => '/usr/bin/chcon -t httpd_sys_content_rw_t /var/www/MISP/app/files',
-  #  subscribe => File['/var/www/MISP/app/files'],
-  #}
-
-  #exec {'chcon files/terms':
-  #  command   => '/usr/bin/chcon -t httpd_sys_content_rw_t /var/www/MISP/app/files/terms',
-  #  subscribe => File['/var/www/MISP/app/files/terms'],
-  #}
-
-  #exec {'chcon files/scripts/tmp':
-  #  command   => '/usr/bin/chcon -t httpd_sys_content_rw_t /var/www/MISP/app/files/scripts/tmp',
-  #  subscribe => File['/var/www/MISP/app/files/scripts/tmp'],
-  #}
-
-  #exec {'chcon CakeResque':
-  #  command   => '/usr/bin/chcon -t httpd_sys_content_rw_t /var/www/MISP/app/Plugin/CakeResque/tmp',
-  #  subscribe => File['/var/www/MISP/app/Plugin/CakeResque/tmp'],
-  #}
-
-  #exec {'chcon app/tmp':
-  #  command   => '/usr/bin/chcon -R -t httpd_sys_content_rw_t /var/www/MISP/app/tmp',
-  #  subscribe => File['/var/www/MISP/app/tmp'],
-  #}
-
-  #exec {'chcon app/webroot/img/orgs':
-  #  command   => '/usr/bin/chcon -R -t httpd_sys_content_rw_t /var/www/MISP/app/webroot/img/orgs',
-  #  subscribe => File['/var/www/MISP/app/webroot/img/orgs'],
-  #}
-
-  #exec {'chcon app/webroot/img/custom':
-  #  command   => '/usr/bin/chcon -R -t httpd_sys_content_rw_t /var/www/MISP/app/webroot/img/custom',
-  #  subscribe => File['/var/www/MISP/app/webroot/img/custom'],
-  #}
-
   exec{'setsebool redis':
     command   => '/usr/sbin/setsebool -P httpd_can_network_connect on',
     subscribe => File['/etc/opt/rh/rh-php56/php.d/99-redis.ini'],
     notify    => Service[$webservername],
   }
-
-  #exec {'restart apache':
-  #  command   => '/bin/systemctl restart  httpd.service',
-  #  user      => 'root',
-  #  group     => 'apache',
-  #  subscribe => Exec['setsebool redis'],
-  #}
-
-  #exec{'chcon config.php':
-  #  command   => '/usr/bin/chcon -t httpd_sys_content_rw_t /var/www/MISP/app/Config/config.php',
-  #  subscribe => File['/var/www/MISP/app/Config/config.php'],
-  #}
 }
