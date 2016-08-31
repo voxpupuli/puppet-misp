4741e8a0751b525e9f74fa9874a0ff89f2be2a8a
Pablo Panero <pablo.panero@cern.ch>
2016-08-31 14:27:22 +0200

    params ordered and changed

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,12 +36,26 @@
 # Copyright 2016 Your name here, unless otherwise noted.
 #
 
-class misp ($db_name = 'misp', $db_user = 'misp', $db_host = 'misp.com', $db_port = '5505', $git_tag='v2.4.51',
-  $salt='Rooraenietu8Eeyo<Qu2eeNfterd-dd+', $cipherseed='',
-  $orgname = 'ORGNAME', $webservername = 'httpd', $email = 'root@localhost', $contact = 'root@localhost',
-  $live = true, $site_admin_debug = false, $enr_service = false, $enr_hover = false,
-  $gnu_email = 'no-reply@localhost', $gnu_homedir = '/var/www/html',
-  $import_service = false, $export_service = false) {
+class misp (
+  $db_name = 'misp',
+  $db_user = 'misp',
+  $db_host = 'misp.com',
+  $db_port = '5505',
+  $git_tag='v2.4.51',
+  $salt='Rooraenietu8Eeyo<Qu2eeNfterd-dd+',
+  $cipherseed='',
+  $orgname = 'ORGNAME',
+  $webservername = 'httpd',
+  $email = 'root@localhost',
+  $contact = 'root@localhost',
+  $live = true,
+  $site_admin_debug = false,
+  $enr_service = false,
+  $enr_hover = false,
+  $gnu_email = 'no-reply@localhost',
+  $gnu_homedir = '/var/www/html',
+  $import_service = false,
+  $export_service = false) {
 
   #certmgr::certificate { 'cert-misp': }
 
