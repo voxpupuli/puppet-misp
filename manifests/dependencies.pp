f1fef64baae1d6410f85a608da94360fe7e474b9
Pablo Panero <pablo.panero@cern.ch>
2016-09-02 17:20:57 +0200

    crypt install with user root

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -24,6 +24,7 @@ class misp::dependencies inherits misp {
   }
 
   exec {'/usr/bin/scl enable rh-php56 "pear install Crypt_GPG"':
+    user    => root,
     command => '/usr/bin/pear install Crypt_GPG',
     creates => '/usr/bin/Crypt_GPG',
     unless  => '/usr/bin/scl enable rh-php56 "pear list | grep Crypt_GPG"',
