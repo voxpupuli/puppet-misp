51c3d9e83dc80521e487e4b25f0d44bb37b0075d
Pablo Panero <pablo.panero@cern.ch>
2016-09-22 14:10:30 +0200

    egroup role parametrized + database password

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,21 +13,23 @@ class misp (
   $db_user = 'misp',
   $db_host = 'localhost',
   $db_port = '3306',
-  $misp_git_tag='v2.4.51',
-  $stix_git_repo='git://github.com/STIXProject/python-stix.git',
-  $stix_git_tag='v1.1.1.4',
-  $cybox_git_repo='git://github.com/CybOXProject/python-cybox.git',
-  $cybox_git_tag='v2.1.0.12',
-  $salt='Rooraenietu8Eeyo<Qu2eeNfterd-dd+',
-  $cipherseed='',
+  $db_password = '',
+  $misp_git_tag = 'v2.4.51',
+  $stix_git_repo = 'git://github.com/STIXProject/python-stix.git',
+  $stix_git_tag = 'v1.1.1.4',
+  $cybox_git_repo = 'git://github.com/CybOXProject/python-cybox.git',
+  $cybox_git_tag = 'v2.1.0.12',
+  $salt = 'Rooraenietu8Eeyo<Qu2eeNfterd-dd+',
+  $cipherseed = '',
+  $auth_method = '',
   $org_id = '1',
   $webservername = 'httpd',
   $email = 'root@localhost', # This address is used as sender (from) when sending notifications
   $contact = 'root@localhost', # This address is used in error messages
   $live = true,
-  $site_admin_debug = false, #TODO: details
-  $enrichment_service = false, #TODO
-  $enrichment_hover = false, #TODO
+  $site_admin_debug = false,
+  $enrichment_service = false,
+  $enrichment_hover = false,
   $gpg_email = 'no-reply@localhost',
   $gpg_homedir = '/var/www/html',
   $import_service = false,
@@ -38,7 +40,9 @@ class misp (
   $default_user = 'apache',
   $default_group = 'apache',
   $default_high_user = 'root',
-  $default_high_group = 'apache') {
+  $default_high_group = 'apache',
+  $shib_default_org = 'ORGNAME',
+  $egroup_role_match = {}) {
 
   #include ::systemd
 
