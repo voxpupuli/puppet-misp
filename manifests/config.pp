4592e93fd8e12f407aace6f36e44575b5f057b50
Liviu Valsan <liviu.valsan@cern.ch>
2016-10-27 15:12:00 +0200

    Fixed source / content mix-up

--- manifests/config.pp
+++ manifests/config.pp
@@ -68,7 +68,7 @@ class misp::config inherits misp {
     ensure    => file,
     owner     => $misp::default_high_user,
     group     => $misp::default_high_group,
-    source    => template('misp/core.php.erb'),
+    content   => template('misp/core.php.erb'),
     subscribe => Exec['Directory permissions'],
   }
 
