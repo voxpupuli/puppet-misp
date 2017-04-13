d316900a11641bd84e52b4ed128f02dd218f7d3a
Pablo Panero <pablo.panero@cern.ch>
2017-04-13 16:05:46 +0200

    changed test to comply with rubocop

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -4,148 +4,146 @@ describe 'misp::config' do
   on_supported_os.each do |os, facts|
     context "on #{os}" do
       let(:facts) do
-
         facts
       end
       it { is_expected.to compile.with_all_deps }
       it { is_expected.to contain_class('Misp::Config').that_requires('Class[Misp::Install]') }
 
       context 'With default values' do
-
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP//app/Plugin/CakeResque/Config/config.php').
-          with_ensure('file').
-          with_owner('root').
-          with_group('apache').
-          with_source(%r{/var/www/MISP/}).
-          that_subscribes_to('Exec[CakeResque install]')
-        }
+            with_ensure('file').
+            with_owner('root').
+            with_group('apache').
+            with_source(%r{/var/www/MISP/}).
+            that_subscribes_to('Exec[CakeResque install]')
+        end
 
-        it {
+        it do
           is_expected.to contain_exec('Directory permissions').
-          with_command(%r{(root).*(apache).*(/var/www/MISP/)}).
-          with_refreshonly('true').
-          that_requires('File[/var/www/MISP//app/Plugin/CakeResque/Config/config.php]').
-          that_subscribes_to('Exec[CakeResque install]')
-        }
+            with_command(%r{(root).*(apache).*(/var/www/MISP/)}).
+            with_refreshonly('true').
+            that_requires('File[/var/www/MISP//app/Plugin/CakeResque/Config/config.php]').
+            that_subscribes_to('Exec[CakeResque install]')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP//app/files').
-          with_ensure('directory').
-          with_owner('apache').
-          with_group('apache').
-          that_subscribes_to('Exec[Directory permissions]').
-          that_notifies(['File[/var/www/MISP//app/files/terms]', 'File[/var/www/MISP//app/files/scripts/tmp]'])
-        }
+            with_ensure('directory').
+            with_owner('apache').
+            with_group('apache').
+            that_subscribes_to('Exec[Directory permissions]').
+            that_notifies(['File[/var/www/MISP//app/files/terms]', 'File[/var/www/MISP//app/files/scripts/tmp]'])
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP//app/files/scripts/tmp').
-          with_ensure('directory').
-          with_owner('apache').
-          with_group('apache')
-        }
+            with_ensure('directory').
+            with_owner('apache').
+            with_group('apache')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP//app/files/terms').
-          with_ensure('directory').
-          with_owner('apache').
-          with_group('apache')
-        }
+            with_ensure('directory').
+            with_owner('apache').
+            with_group('apache')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP//app/Plugin/CakeResque/tmp').
-          with_ensure('directory').
-          with_owner('apache').
-          with_group('apache').
-          that_subscribes_to('Exec[Directory permissions]')
-        }
+            with_ensure('directory').
+            with_owner('apache').
+            with_group('apache').
+            that_subscribes_to('Exec[Directory permissions]')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP//app/tmp').
-          with_ensure('directory').
-          with_owner('apache').
-          with_group('apache').
-          that_subscribes_to('Exec[Directory permissions]').
-          that_notifies('File[/var/www/MISP//app/tmp/logs/]')
-        }
+            with_ensure('directory').
+            with_owner('apache').
+            with_group('apache').
+            that_subscribes_to('Exec[Directory permissions]').
+            that_notifies('File[/var/www/MISP//app/tmp/logs/]')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP//app/webroot/img/orgs').
-          with_ensure('directory').
-          with_owner('apache').
-          with_group('apache').
-          that_subscribes_to('Exec[Directory permissions]').
-          that_notifies('File[/var/www/MISP//app/tmp/logs/]')
-        }
+            with_ensure('directory').
+            with_owner('apache').
+            with_group('apache').
+            that_subscribes_to('Exec[Directory permissions]').
+            that_notifies('File[/var/www/MISP//app/tmp/logs/]')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP//app/webroot/img/custom').
-          with_ensure('directory').
-          with_owner('apache').
-          with_group('apache').
-          that_subscribes_to('Exec[Directory permissions]').
-          that_notifies('File[/var/www/MISP//app/tmp/logs/]')
-        }
+            with_ensure('directory').
+            with_owner('apache').
+            with_group('apache').
+            that_subscribes_to('Exec[Directory permissions]').
+            that_notifies('File[/var/www/MISP//app/tmp/logs/]')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP//app/tmp/logs/').
-          with_ensure('directory').
-          with_owner('apache').
-          with_group('apache')
-        }
+            with_ensure('directory').
+            with_owner('apache').
+            with_group('apache')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP/app/Config//bootstrap.php').
-          with_ensure('file').
-          with_owner('root').
-          with_group('apache').
-          that_subscribes_to('Exec[Directory permissions]')
-        }
+            with_ensure('file').
+            with_owner('root').
+            with_group('apache').
+            that_subscribes_to('Exec[Directory permissions]')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP/app/Config//core.php').
-          with_ensure('file').
-          with_owner('root').
-          with_group('apache'). 
-          that_subscribes_to('Exec[Directory permissions]')
-        }
+            with_ensure('file').
+            with_owner('root').
+            with_group('apache').
+            that_subscribes_to('Exec[Directory permissions]')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP/app/Config//config.php').
-          with_ensure('file').
-          with_owner('apache').
-          with_group('apache').
-          that_subscribes_to('Exec[Directory permissions]')
-        }
+            with_ensure('file').
+            with_owner('apache').
+            with_group('apache').
+            that_subscribes_to('Exec[Directory permissions]')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP/app/Config//database.php').
-          with_ensure('file').
-          with_owner('root').
-          with_group('apache').
-          that_subscribes_to('Exec[Directory permissions]')
-        }
+            with_ensure('file').
+            with_owner('root').
+            with_group('apache').
+            that_subscribes_to('Exec[Directory permissions]')
+        end
 
-        it {
+        it do
           is_expected.to contain_exec('setsebool redis').
-          that_subscribes_to('File[/etc/opt/rh/rh-php56/php.d/99-redis.ini]')
-        }
+            that_subscribes_to('File[/etc/opt/rh/rh-php56/php.d/99-redis.ini]')
+        end
 
-        it {
+        it do
           is_expected.to contain_file('/var/www/MISP//app/Console/worker/start.sh').
-          with_owner('root').
-          with_group('apache')
-        }
+            with_owner('root').
+            with_group('apache')
+        end
       end
       context 'With webserver defined' do
-        let(:pre_condition) {
+        let(:pre_condition) do
           'service{"httpd":}'
-        }
+        end
 
-        it {
+        it do
           is_expected.to contain_exec('setsebool redis').
-          that_notifies('Service[httpd]')
-        }
+            that_notifies('Service[httpd]')
+        end
       end
     end
   end
--- spec/classes/service_spec.rb
+++ spec/classes/service_spec.rb
@@ -9,41 +9,41 @@ describe 'misp::service' do
       it { is_expected.to compile.with_all_deps }
       it { is_expected.to contain_class('Misp::Service').that_requires('Class[Misp::Config]') }
 
-      it {
+      it do
         is_expected.to contain_service('rh-php56-php-fpm').
-        with_ensure('running').
-        with_enable('true')
-      }
+          with_ensure('running').
+          with_enable('true')
+      end
 
-      it {
+      it do
         is_expected.to contain_service('haveged').
-        with_ensure('running').
-        with_enable('true')
-      }
+          with_ensure('running').
+          with_enable('true')
+      end
 
-      it {
+      it do
         is_expected.to contain_service('redis').
-        with_ensure('running').
-        with_enable('true')
-      }
+          with_ensure('running').
+          with_enable('true')
+      end
 
       context 'With default values' do
-        it {
+        it do
           is_expected.to contain_exec('start bg workers').
-          with_command(%r{(apache).*(/var/www/MISP/)}).
-          with_unless(%r{(apache).*(/var/www/MISP/)}).
-          with_user('root').
-          with_group('apache')
-        }
+            with_command(%r{(apache).*(/var/www/MISP/)}).
+            with_unless(%r{(apache).*(/var/www/MISP/)}).
+            with_user('root').
+            with_group('apache')
+        end
 
-        it {
+        it do
           is_expected.to contain_exec('restart bg workers').
-          with_command(%r{(apache).*(/var/www/MISP/)}).
-          with_user('root').
-          with_group('apache').
-          with_refreshonly('true').
-          that_subscribes_to('Exec[CakeResque install]')
-        }
+            with_command(%r{(apache).*(/var/www/MISP/)}).
+            with_user('root').
+            with_group('apache').
+            with_refreshonly('true').
+            that_subscribes_to('Exec[CakeResque install]')
+        end
       end
     end
   end
