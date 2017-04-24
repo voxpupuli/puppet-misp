5fc811fa5cc94d482283584592871ff9fc24a06f
Pablo Panero <pablo.panero@cern.ch>
2017-04-24 09:29:36 +0200

    Deleted pip install importlib. It is merged into python 2.7. Take into account if supporting != centos 7

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -21,10 +21,4 @@ class misp::dependencies inherits misp {
       ensure => present,
     }
   }
-  
-  exec {'pip install importlib':
-    require => Package[python2-pip],
-    command => '/usr/bin/pip install importlib',
-    unless  => '/usr/bin/pip list | grep importlib',
-  }
 }
